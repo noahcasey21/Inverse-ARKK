d87f16129e41899cd07644f6146d6788f54eb66d
Noah Casey <nccm2d@umsystem.edu>
2023-09-08 15:03:50 -0500

    Initialized Django framework, created venv and Docker, began production of web scraper

---
+++ README.md
@@ -0,0 +1,4 @@
+# Inverse ARKK ETF
+
+The goal is to eventually create a program that pulls ARKK trades from the web and 
+uses them to create simulated opposing trades to track equity. 
--- backtest.py
+++ backtest.py
@@ -1,50 +1,68 @@
-#setup historical prices dataframe that adds row daily for fast lookup in equity 
+import pandas as pd
+import yfinance as yf
 
-holdings = {} #{"SPY":{"Pct":x, "Price":y}, ...}
+class TradingEngine:
 
-date = None
-base = 1
+	def __init__(self, df):
+		#setup equity
+		self.init_equity()
 
-for i, data in arkk.iterrows():
-	if date != data['Date']:
-		date = data['Date']
-		base = equity[date - 1]
+	def forwardtest(self, df):
+		...
 
-		#load day's trades 
+	#setup historical prices dataframe that adds row daily for fast lookup in equity 
+	def backtest(self, df):
+		holdings = {} #{"SPY":{"Pct":x, "Price":y}, ...}
+		equity = pd.Series()
 
-    ticker = data['Ticker']
+		date = None
+		base = 1
 
-    #recalculate equity
-    for holding, info in holdings:
-        #not correct: math needs to be fixed to account for daily price change as well as average price
-        dif = yf.download(ticker, date)['Close']
-        change = info['Pct'] * dif / info['Price']
-        base += change
-        
+		for i, data in df.iterrows():
+			if date != data['Date']:
+				date = data['Date']
+				base = equity[date - 1]
 
-	#check new trades
-	if data['Buy']:
-		if ticker in holdings:
-			#calculate average buy
-			holdings[ticker]["Price"] = (data['Percent'] * base * yf.download(ticker, date)['Close'] + holdings[ticker]["Pct"] * holdings[ticker]["Price"]) / 
-				(holdings[ticker]["Pct"] + data['Percent'] * equity)
+				#load day's trades 
+			ticker = data['Ticker']
 
-			holdings[ticker]["Pct"] = holdings[ticker]["Pct"] + data['Percent'] * base
-			
-			
-		else:
-			holdings[ticker] = {"Pct":base * data["Percent"], 
-							"Price" = yf.download(ticker, date)['Close']}
+			#recalculate equity
+			for holding, info in holdings:
+				#not correct: math needs to be fixed to account for daily price change as well as average price
+				dif = yf.download(ticker, date)['Close']
+				change = info['Pct'] * dif / info['Price']
+				base += change
+				
 
-	elif data['Sell']:
-				holdings[ticker]["Pct"] -= (base * data['Percent']) #all that is needed here?
+			#check new trades
+			if data['Buy']:
+				if ticker in holdings:
+					#calculate average buy
+					holdings[ticker]["Price"] = (data['Percent'] * base * yf.download(ticker, date)['Close'] + holdings[ticker]["Pct"] * holdings[ticker]["Price"]) / \
+						(holdings[ticker]["Pct"] + data['Percent'] * base)
 
-	else:
-		log(f'Buy/Sell issue at row {i}')
+					holdings[ticker]["Pct"] = holdings[ticker]["Pct"] + data['Percent'] * base
+					
+					
+				else:
+					holdings[ticker] = {"Pct":base * data["Percent"], "Price": yf.download(ticker, date)['Close']}
+
+			elif data['Sell']:
+						holdings[ticker]["Pct"] -= (base * data['Percent']) #all that is needed here?
+
+			else:
+				self.log(f'Buy/Sell issue at row {i}')
 
 
-def log(string):
-	#log data issues to go clean
-	f = open('data_issues.txt','a')
-	f.write(string)
-	f.close()
+	def log(self, string):
+		#log data issues to go clean
+		f = open('data_issues.txt','a')
+		f.write(string)
+		f.close()
+
+	def init_equity(self):
+		start = self.df['Date'].min()
+		end = self.df['Date'].max()
+
+		self.equity = pd.Series(index=pd.date_range(start, end))
+
