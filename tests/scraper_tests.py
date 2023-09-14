781148ee39368f1739574217db2deff0981ddab2
Noah Casey <nccm2d@umsystem.edu>
2023-09-13 21:35:05 -0500

    Started test modules

---
+++ tests/scraper_tests.py
@@ -0,0 +1,14 @@
+import unittest
+import pandas as pd
+
+class TestScraper(unittest.TestCase):
+    
+    def test_general_scrape(self):
+
+        self.assertTrue()
+
+    def test_date_scrape():
+        ...
+
+if __name__ == "__main__":
+    unittest.main()
